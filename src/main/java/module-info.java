94da3acfd095963364bac72cc1fe387e98430416
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-06-24 16:07:13 +0300

    .

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,6 @@
 module com.tugalsan.api.sql.duplicate {
     requires java.sql;
-    requires com.tugalsan.api.runnable;
+    requires com.tugalsan.api.callable;
     requires com.tugalsan.api.string;
     requires com.tugalsan.api.tuple;
     requires com.tugalsan.api.sql.cellgen;
